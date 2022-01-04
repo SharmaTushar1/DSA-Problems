610d7b141034c0578191cb75307e6134a628ea37
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-01-04 21:00:08 +0530

    Time: 2 ms (98.18%), Space: 96.9 MB (39.57%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -2,38 +2,40 @@ class Solution {
     public int firstMissingPositive(int[] arr) {
         int i = 0; 
         int n = arr.length;
+        
+        //first sort the array so that the positive elements are in their indices position
+        //eg-[3,4,-1,1]
+        
         while (i<n) {
             int correct = arr[i]-1;
-            if (correct<n && correct >=0 && (arr[i]!=arr[correct] && arr[i]>=1)) {
+            if (correct<n && correct >=0 && (arr[i]!=arr[correct] && arr[i]>=1)) { //ignore the elements that are negative or bigger than the length (as they don't have any position in the sorted array)
                     int temp = arr[i];
                     arr[i] = arr[correct];
                     arr[correct] = temp;
                 } else i++;
         }
         
-        // int min =Integer.MAX_VALUE;
-        int min = 1;
-        System.out.println(Arrays.toString(arr));
+        //eg-[1,-1,3,4]
+        
+        
+        //return the index which is not present
+        //eg-[1,-1,3,4] this will return 2 as -1!=j+1(1+1=2) which is the required answer
         
         for (int j = 0; j<n; j++) {
-            // if (arr[j]>=1 && arr[j]<n) {
                 if (arr[j]!=j+1) {
                     return j+1;
-                // }
             }
         }
         
-//         int temp = min;
+        //for arrays like [1,2,3,4] or [3,2,1] (all elements = indices) or so we need to make a min var and increment it till we don't find required index
+        //eg-[1,2,3,4]
+        
+        int min = 1; //least positive number
         
         for (int f = 0; f<n; f++) {
-            if (arr[f]==min) min ++;
+            if (arr[f]==min) min++;
         }
         
-        return min;
-        
-        
-        
-        
-        
+        return min; 
     }
 }
