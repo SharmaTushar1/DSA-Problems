3caf22644ad755f3ea76503e8f2d75dddcf1aa99
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-01-05 18:45:38 +0530

    Time: 4 ms (85.45%), Space: 39.1 MB (58.38%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.java
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean judgeCircle(String moves) {
         int l = 0, r = 0, d = 0, u = 0;
-        for (char c: moves.toCharArray()) {
-            if (c=='U') u++;
-            else if (c == 'D') d++;
-            else if (c == 'R') r++;
-            else if (c=='L') l++;
-        }
+        
+            for (char c: moves.toCharArray()) {
+                switch (c) {
+                    case 'U' -> u++;
+                    case 'D' -> d++;
+                    case 'R' -> r++;
+                    case 'L' -> l++;
+                }
+            }
         // System.out.println("l is = "+l+" r is = " + r);
         if (l==r && u==d) return true;
         return false;
