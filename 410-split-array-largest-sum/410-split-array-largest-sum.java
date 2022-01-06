a0a26438149ef9965cd6e95f15d88834f9e767ec
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-01-06 22:26:47 +0530

    Time: 2 ms (38.82%), Space: 38.5 MB (18.51%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.java
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -2,33 +2,27 @@ class Solution {
     public int splitArray(int[] nums, int m) {
         int start = 0;
         int end = 0;
-        for (int i = 0; i<nums.length; i++) {
-            start = Math.max(start, nums[i]);//ans for m=n
-            end += nums[i];//ans for n=1
+        for (int i =0; i<nums.length; i++) {
+            start = Math.max(start,nums[i]);
+            end += nums[i];
         }
+        
+        //binary search
         
         while (start<end) {
-            
             int mid = start+(end-start)/2;
-  int sum = 0;
-        int pieces = 1;
-        
-        for (int n: nums) {
-            if (sum + n>mid) {
-                pieces++;
-                sum = n;
-            } else {
-                sum+=n;
+            int sum = 0;
+            int pieces = 1;
+            for (int num: nums) {
+                if (sum+num>mid) { //start a new subarray
+                    pieces++;//next piece started
+                    sum = num; //new subarray started
+                } else sum+=num;
             }
-        }
             if (pieces>m) start = mid+1;
             else end = mid;
-            
         }
-        
-        
-        
-
         return end;
+        
     }
 }
