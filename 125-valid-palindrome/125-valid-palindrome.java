0f48a8fc8c0b5af0e54736899ff1b2857e01fd53
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-02-13 00:29:49 +0530

    Time: 4 ms (77.37%), Space: 43.6 MB (29.51%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int start = 0; 
+        int end = s.length()-1;
+        s = s.toLowerCase();
+        
+        while (start<end) {
+            while (start<end && !isAlpha(s.charAt(start))) start++;
+            while (start<end && !isAlpha(s.charAt(end))) end--;
+                if (s.charAt(start)!=s.charAt(end)) return false;
+                else {
+                    start++;
+                    end--;
+                // }
+            }
+        }
+        return true;
+        
+    }
+    
+    public boolean isAlpha(char c) {
+        return (c>='0' && c<='9') || (c>='a' && c<='z');
+    }
+    
+}
