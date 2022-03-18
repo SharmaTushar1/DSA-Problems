161bf0af41ce3aee87f619f0f7d1edf235269491
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-03-18 20:04:36 +0530

    Time: 4 ms (59.65%), Space: 54.1 MB (35.96%) - LeetHub

--- 654-maximum-binary-tree/654-maximum-binary-tree.java
+++ 654-maximum-binary-tree/654-maximum-binary-tree.java
@@ -15,37 +15,24 @@
  */
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return helper(nums, 0, nums.length-1);
+        return construct(nums, 0, nums.length);
     }
-    
-    public TreeNode helper(int[] nums, int start, int end) {
-        
-        if (start>end) return null;
-        
-        int maxInd = maxIndex(nums, start, end);
-        
-        System.out.println(nums[maxInd]);
-        
-        TreeNode node = new TreeNode(nums[maxInd]);
-        
-        node.left = helper(nums, start, maxInd-1);
-        
-        node.right = helper(nums, maxInd+1, end);
-        
-        return node;
-        
+    public TreeNode construct(int[] nums, int l, int r) {
+        if (l == r)
+            return null;
+        int max_i = max(nums, l, r);
+        TreeNode root = new TreeNode(nums[max_i]);
+        root.left = construct(nums, l, max_i);
+        root.right = construct(nums, max_i + 1, r);
+        return root;
     }
-    
-    public int maxIndex(int[] nums, int start, int end) {
-        int maxValue = -999;
-        int maxIndex = 0;
-        for (int i = start; i<=end; i++) {
-            if (nums[i]>maxValue) {
-                maxIndex = i;
-                maxValue = nums[maxIndex];
-            }
+    public int max(int[] nums, int l, int r) {
+        int max_i = l;
+        for (int i = l; i < r; i++) {
+            if (nums[max_i] < nums[i])
+                max_i = i;
         }
-        return maxIndex;
+        return max_i;
     }
     
 }
