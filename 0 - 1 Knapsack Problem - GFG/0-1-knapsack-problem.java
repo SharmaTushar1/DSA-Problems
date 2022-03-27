d456db80143cb283aac7dbae9084e9e9f1defbb2
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-03-27 18:23:19 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -50,42 +50,21 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
+        
         int[][] dp = new int[n+1][W+1];
         
         for (int i = 0; i<=n; i++) {
-            for (int j = 0; j<=W; j++) {
-                if (i == 0 || j == 0) dp[i][j] = 0;
-                else if (wt[i-1]<=j) {
-                    dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+            for (int w = 0; w<=W; w++) {//w = weight of current 
+                if (i == 0 || w == 0) dp[i][w] = 0;
+                else if (w>=wt[i-1]) {
+                    dp[i][w] = Math.max(val[i-1]+dp[i-1][w-wt[i-1]], dp[i-1][w]);
+                } else {
+                    dp[i][w] = dp[i-1][w];
                 }
-                else dp[i][j] = dp[i-1][j];
             }
-            
         }
         return dp[n][W];
-        /*
-        int i, w;
-        int K[][] = new int[n + 1][W + 1];
-        for (i = 0; i <= n; i++) 
-        {
-            for (w = 0; w <= W; w++) 
-            {
-                if (i == 0 || w == 0)
-                    K[i][w] = 0;
-                else if (wt[i - 1] <= w)
-                    K[i][w]
-                        = Math.max(val[i - 1]
-                         + K[i - 1][w - wt[i - 1]],
-                         K[i - 1][w]);
-                else
-                    K[i][w] = K[i - 1][w];
-            }
-        }
-  
-        return K[n][W];
-        */
     } 
-    
 }
 
 
