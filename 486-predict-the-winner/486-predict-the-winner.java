af7ff23e6bbf47b2b15077322d2a850454bf90d5
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-03-30 22:44:50 +0530

    Time: 1 ms (81.19%), Space: 41.5 MB (55.35%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.java
+++ 486-predict-the-winner/486-predict-the-winner.java
@@ -4,7 +4,11 @@ class Solution {
         
         int start = 0;
         int end = nums.length-1;
-        int p1Score = helper(start, nums, end);
+        int[][] dp = new int[nums.length+1][nums.length+1];
+        for (int[] arr: dp) {
+            Arrays.fill(arr, -1);
+        }
+        int p1Score = helper(start, nums, end, dp);
         int totalScore = 0;
         for (int num: nums) {
             totalScore += num;
@@ -13,15 +17,16 @@ class Solution {
         
     }
     
-    public int helper(int start, int[] nums, int end) {
+    public int helper(int start, int[] nums, int end, int[][] dp) {
         if (start>end) return 0;
         else if (start==end) return nums[start];
+        else if (dp[start][end]!=-1) return dp[start][end];
         else {
-            int p1Score = Math.max(
-                nums[start]+Math.min(helper(start+2, nums, end), helper(start+1, nums, end-1)), 
-                nums[end]+Math.min(helper(start+1, nums, end-1), helper(start, nums, end-2))
+            dp[start][end] = Math.max(
+                nums[start]+Math.min(helper(start+2, nums, end, dp), helper(start+1, nums, end-1, dp)), 
+                nums[end]+Math.min(helper(start+1, nums, end-1, dp), helper(start, nums, end-2, dp))
             );
-            return p1Score;
+            return dp[start][end];
         }
     }
     
