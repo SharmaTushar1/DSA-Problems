9de0aea00ba14206e879d88aa7518383e331e790
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-03-31 18:04:55 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (34.59%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -3,10 +3,10 @@ class Solution {
         int loot1 = 0;
         int loot2 = 0;
         for (int i = 0; i<nums.length; i++) {
-            int totalLoot = Math.max(nums[i]+loot1, loot2);
+            int loot = Math.max(loot2, loot1+nums[i]);
             loot1 = loot2;
-            loot2 = totalLoot;
+            loot2 = loot;
         }
         return loot2;
-    }
+    }   
 }
