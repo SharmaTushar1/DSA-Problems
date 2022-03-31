a90dfb12d819a001c354098db1274bf970e80049
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-03-31 23:51:19 +0530

    Time: 11 ms (53.39%), Space: 47.3 MB (35.27%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return wordBreakMemo(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);
+    }
+
+    private boolean wordBreakMemo(String s, Set<String> wordDict, int start, Boolean[] memo) {
+        if (start == s.length()) {
+            return true;
+        }
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && wordBreakMemo(s, wordDict, end, memo)) {
+                return memo[start] = true;
+            }
+        }
+        return memo[start] = false;
+    }
+}
