d9f9ec7590bac78681ef837f106d95b05bc2a528
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-01 15:22:35 +0530

    Time: 2 ms (87.15%), Space: 42.8 MB (52.66%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,39 +1,16 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int i = a.length()-1;
-        int j = b.length()-1;
-        char c1 = '0';
-        char c2 = '0';
+        int i = a.length() -1 , j = b.length()-1;
         int carry = 0;
-        StringBuilder s = new StringBuilder();
-        while (i>=0 || j>=0 ) {
-            if (i<0 || j<0) {
-                if (j<0) {c2 = '0'; c1 = a.charAt(i);}
-                else if (i<0) {c1 = '0'; c2 = b.charAt(j);}
-            } else {
-                c1 = a.charAt(i);
-                c2 = b.charAt(j);
-            }
-            // System.out.println(c1+" "+c2);
-            if (c1 == '0' && c2 == '0') {
-                int add = carry%2;
-                s.append(add);
-                carry = 0;
-            } else if (c1=='0' || c2 == '0') {
-                int add = (carry+1)%2;
-                s.append(add);
-                carry = add==0?1:0;
-            } else {
-                int add = carry%2;
-                s.append(add);
-                carry = 1;
-            }
-            i--;
-            j--;
+        StringBuilder sb = new StringBuilder();
+        while (i>=0 || j>=0) {
+            int sum = carry;
+            if (i>=0) sum += a.charAt(i--) - '0';//111 (a)
+            if (j>=0) sum += b.charAt(j--) - '0';//111 (b)
+            sb.append(sum%2);
+            carry = sum/2;
         }
-            if (carry!=0) s.append(carry);
-        
-        return s.reverse().toString();
-        
+        if (carry!=0) sb.append(carry);
+        return sb.reverse().toString();
     }
 }
