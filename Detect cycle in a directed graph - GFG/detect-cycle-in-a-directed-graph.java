1e83294653f0899e13a083db67c3d4e08d41965a
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-03 18:57:47 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V + 1; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] vis = new boolean[V];
+        boolean[] arr = new boolean[V];
+        for (int i = 0; i<V; i++) {
+            if (isCycle(i, adj, vis, arr)) return true;
+        }
+        return false;
+    }
+    public boolean isCycle(int i, ArrayList<ArrayList<Integer>> adj, boolean[] vis, boolean[] arr) {
+        vis[i] = true;
+        arr[i] = true;
+        for (int it:adj.get(i)) {
+            if (!vis[it]) {
+                if (isCycle(it, adj, vis, arr)) return true;
+            } else if (arr[it]) return true;
+        }
+        arr[i] = false;
+        return false;
+    }
+}
