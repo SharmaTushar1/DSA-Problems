d6e16f198fc6ca891b1b58b9d7adb48ec738729b
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-03 10:02:10 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.java
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -35,21 +35,21 @@ class GFG {
 class Solution {
     // Function to return Breadth First Traversal of given graph.
     public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean vis[] = new boolean[V];
-        ArrayList<Integer> bfs = new ArrayList<Integer>();
+        boolean[] vis = new boolean[V];
+        ArrayList<Integer> list = new ArrayList<>();
+        vis[0] = true;
         Queue<Integer> q = new LinkedList<>();
         q.add(0);
-        vis[0] = true;
         while (!q.isEmpty()) {
             Integer node = q.poll();
-            bfs.add(node);
+            list.add(node);
             for (Integer it: adj.get(node)) {
                 if (!vis[it]) {
                     vis[it] = true;
                     q.add(it);
-                }
+                } 
             } 
         }
-        return bfs;
+        return list;
     }
 }
