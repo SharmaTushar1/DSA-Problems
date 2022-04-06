9fbd3bf4f0a1b2bec26c2abf5a4ed3ab96765a22
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-06 19:23:07 +0530

    Time: 34 ms (50.34%), Space: 58.8 MB (63.43%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,39 +1,24 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-        
-        Arrays.sort(arr);
-        
-        List<List<Integer>> ls = new ArrayList<>();
-        
-        for (int i = 0; i<arr.length-2; i++) {
-            if (i == 0 || (i>0 && arr[i-1]!=arr[i])) {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i<nums.length-2; i++) {
+            if (nums[i]>0) break;
+            if (i>0 && nums[i]==nums[i-1]) continue; //I've to do this atleast once that's why i==i-1 instead of i+1==i
             int start = i+1;
-            int end = arr.length-1;
-                
-            int sum = 0-arr[i];
-            
-            
+            int end = nums.length-1;
             while (start<end) {
-                // List<Integer> list = new ArrayList();
-                // int sum = ;
-                if (arr[start] + arr[end] == sum) {
-                    ls.add(Arrays.asList(arr[i], arr[start], arr[end]));
-                    
-                    while (start<end && arr[start] == arr[start+1]) start++;
-                    while (start<end && arr[end] == arr[end-1]) end--;
-                    
+                if (nums[start]+nums[end]+nums[i]>0) end--;
+                else if (nums[start]+nums[end]+nums[i]<0) start++;
+                else {
+                    ans.add(Arrays.asList(nums[i], nums[start], nums[end]));
                     start++;
                     end--;
-                } else if (arr[start] + arr[end] < sum) {
-                    start++;
-                } else end--;
-                // if (list.size()!=0) ls.add(list);
-            }
+                    while (start<end && nums[start]==nums[start-1]) start++;
+                    while (start<end && nums[end] == nums[end+1]) end--;
+                }
             }
         }
-        
-        return ls;
+        return ans;
     }
-    
-    
 }
