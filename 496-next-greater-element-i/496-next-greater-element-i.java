499ec3c48efadf780eeb429f151e957d02a3160d
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-15 17:05:15 +0530

    Time: 3 ms (84.04%), Space: 42.7 MB (81.54%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        for (int i = 0; i<nums1.length; i++) {
-            int cur = nums1[i];
-            boolean flag = false;
-            boolean updated = false;
-            for (int j = 0; j<nums2.length; j++) {
-                if (cur == nums2[j] && !flag) flag = true;
-                if (flag && nums2[j]>cur) {
-                    nums1[i] = nums2[j];
-                    updated = true;
-                    break;
-                }
-            }
-            if (!updated) nums1[i] = -1;
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        
+        for (int i = 0; i<nums2.length; i++) {
+            while (!stack.isEmpty() && stack.peek()<nums2[i]) 
+                hash.put(stack.pop(), nums2[i]);
+            stack.add(nums2[i]);
         }
+        
+        for (int i = 0; i<nums1.length; i++) {
+            nums1[i] = hash.getOrDefault(nums1[i], -1);
+        }
+        
         return nums1;
+        
     }
 }
