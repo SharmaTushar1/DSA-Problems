d4d0d563b645e42e5a0f8d0a854415437db437d4
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-22 17:33:39 +0530

    Time: 1 ms (89.39%), Space: 41.6 MB (98.00%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -15,21 +15,15 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return (dfsHeight(root) != -1);
+        return height(root)!=-1;
     }
     
-    //the idea is to check whether the tree is balanced or not for that we can find the height if it's not balanced
-    //return -1 other wise return the height
-    //we can find that by checking whether the abs(Math.max(leftHeight, rightHeight))<1 or not
-    
-    public int dfsHeight(TreeNode root) {
+    public int height(TreeNode root) {
         if (root == null) return 0;
-        int lHeight = dfsHeight(root.left);
-        if (lHeight == -1) return -1;
-        int rHeight = dfsHeight(root.right);
-        if (rHeight == -1) return -1;
-        if (Math.abs(lHeight-rHeight)>1) return -1;
-        return 1 + Math.max(lHeight, rHeight);
+        int lHeight = height(root.left);
+        int rHeight = height(root.right);
+        if (Math.abs(lHeight-rHeight)>1 || lHeight == -1 || rHeight == -1) return -1;
+        return 1+Math.max(lHeight, rHeight);
     }
     
 }
