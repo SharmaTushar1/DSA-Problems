d8c06f054b4668f41c74190a79c7346d296e6b04
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-23 16:16:05 +0530

    Time: 10 ms (25.29%), Space: 44.2 MB (14.57%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -3,7 +3,8 @@ public class Codec {
     public String helper;
     public String encode(String longUrl) {
         helper = longUrl;
-        return "";    
+        double rand = Math.random()*9999;
+        return "http://tinyurl.com/"+rand;    
     }
 
     // Decodes a shortened URL to its original URL.
