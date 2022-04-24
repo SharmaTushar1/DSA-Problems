8592a01a8048d1f0fa260f70d9951b5a9e69a0f4
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-24 18:48:37 +0530

    Time: 13 ms (74.14%), Space: 50.5 MB (44.09%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> hash = new HashMap<>();
-        PriorityQueue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
-        for (int i =0; i<nums.length; i++) {
-            hash.put(nums[i], hash.getOrDefault(nums[i], 0)+1);
+        int[] ans = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
-        for (Map.Entry<Integer, Integer> entry: hash.entrySet()) {
-            q.add(entry);
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+        for (Map.Entry<Integer, Integer> m: map.entrySet()) {
+            maxHeap.add(m);
         }
-        int[] arr = new int[k];
         for (int i = 0; i<k; i++) {
-            arr[i] = q.poll().getKey();
+            ans[i] = maxHeap.poll().getKey();
         }
-        return arr;
+        return ans;
     }
 }
