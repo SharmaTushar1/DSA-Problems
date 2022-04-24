538402a16cba22edbfc2f090946e644372b8d6e8
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-24 15:50:05 +0530

    Time: 6 ms (53.45%), Space: 43.7 MB (52.12%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length()!=t.length()) return false;
-        int[] c = new int[26];
+        int[] words = new int[26];
         for (int i = 0; i<s.length(); i++) {
-            c[s.charAt(i)-'a']++;
-            c[t.charAt(i)-'a']--;
+            words[s.charAt(i)-'a']++;
+            words[t.charAt(i)-'a']--;
         }
-        for (int i = 0; i<26; i++) if (c[i]!=0) return false;
+        for (int i = 0; i<=25; i++) {
+            if (words[i]!=0) return false;
+        }
         return true;
     }
 }
