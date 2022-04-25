f576671e3b2f5d21d459753572f29262da5a72c1
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-25 17:52:16 +0530

    Time: 27 ms (72.38%), Space: 58.8 MB (57.20%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,18 +3,14 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i<nums.length-2; i++) {
-            //remove duplicate values for i
             if (i==0||(i>0 && nums[i-1]!=nums[i])) {
                 int j = i+1;
                 int k = nums.length-1;
                 while (j<k) {
-                    List<Integer> list = new ArrayList<>();
                     if (nums[j]+nums[k]+nums[i]>0) k--;
                     else if (nums[j]+nums[k]+nums[i]<0) j++;
-                    //given condition
                     else if (i!=j && i!=k && j!=k && nums[i]+nums[j]+nums[k]==0) {
                         ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                        //remove duplicate values for j and k
                         while (j<k && nums[j]==nums[j+1]) j++;
                         while (k>j && nums[k]==nums[k-1]) k--;
                         j++;
