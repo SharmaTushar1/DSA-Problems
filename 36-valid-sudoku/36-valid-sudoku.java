de5c45a81dbe652dcbd37bd31e932340715bfaf1
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-25 13:19:09 +0530

    Time: 28 ms (11.54%), Space: 48.6 MB (11.35%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> set = new HashSet<String>();
+        HashSet<String> h = new HashSet<>();
         for (int i = 0; i<9; i++) {
             for (int j = 0; j<9; j++) {
                 if (board[i][j]!='.') {
-                    if (!set.add(board[i][j] + "was found in row" + i) ||
-                    !set.add(board[i][j] + "was found in column" + j) ||
-                    !set.add(board[i][j] + "was found in box" + i/3+""+j/3)) return false;
+                    if (!h.add(board[i][j] + "not in row" + i) ||
+                        !h.add(board[i][j]  + "not in column" + j) ||
+                        !h.add(board[i][j] + "not in box" + i/3 + " " + j/3)) return false;
                 }
             }
         }
