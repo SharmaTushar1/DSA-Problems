45d86f3ae3e782b9c384cba93c76362d781cff07
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-25 17:52:47 +0530

    Time: 24 ms (85.10%), Space: 46.7 MB (82.23%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,16 +3,16 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i<nums.length-2; i++) {
-            if (i==0||(i>0 && nums[i-1]!=nums[i])) {
+            if (i==0 || (i>0 && nums[i-1]!=nums[i])) {
                 int j = i+1;
                 int k = nums.length-1;
                 while (j<k) {
-                    if (nums[j]+nums[k]+nums[i]>0) k--;
-                    else if (nums[j]+nums[k]+nums[i]<0) j++;
+                    if (nums[i]+nums[j]+nums[k]>0) k--;
+                    else if (nums[i]+nums[j]+nums[k]<0) j++;
                     else if (i!=j && i!=k && j!=k && nums[i]+nums[j]+nums[k]==0) {
                         ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                        while (j<k && nums[j]==nums[j+1]) j++;
-                        while (k>j && nums[k]==nums[k-1]) k--;
+                        while (j<k && nums[j+1]==nums[j]) j++;
+                        while (k>j && nums[k-1]==nums[k]) k--;
                         j++;
                         k--;
                     }
