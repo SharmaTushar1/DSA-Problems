fcc349f31a360c3110c74f40cff37f3866f66899
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-04-25 14:35:30 +0530

    Time: 5 ms (66.11%), Space: 44 MB (42.50%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i<s.length(); i++) {
-            if (Character.isLetterOrDigit(s.charAt(i))) {
-                sb.append(s.charAt(i));
-            }
-        }
-        int i = 0;
-        int j = sb.length()-1;
+        int i = 0, j = s.length()-1;
         while (i<j) {
-            if (Character.toLowerCase(sb.charAt(i++))!=Character.toLowerCase(sb.charAt(j--))) return false;
+            if (Character.isLetterOrDigit(s.charAt(i)) && Character.isLetterOrDigit(s.charAt(j))) 
+                if (Character.toLowerCase(s.charAt(i++))!=Character.toLowerCase(s.charAt(j--))) return false;
+            while (i<s.length() && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (j>=0 && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if (i<s.length() && j>=0 && !Character.isLetterOrDigit(s.charAt(i)) && !Character.isLetterOrDigit(s.charAt(j))) {
+                i++;
+                j--;
+            }
         }
         return true;
     }
