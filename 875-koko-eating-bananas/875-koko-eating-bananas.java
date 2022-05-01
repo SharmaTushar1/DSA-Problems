71227c06d6b564826180ce328be08a99fd54b412
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-01 14:15:43 +0530

    Time: 42 ms (37.54%), Space: 54.7 MB (11.85%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,23 +1,33 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int start = 1;
-        int end = Integer.MIN_VALUE;
-        for (int pile: piles) end = Math.max(end, pile);
-        while (start<end) {
-            int mid = start+(end-start)/2;
-            if (time(mid, piles, h)>h) start = mid+1;
-            else end = mid;
+        // Initalize the left and right boundaries 
+        int left = 1, right = 1;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
         }
-        return end;
-    }
-    
-    public int time(int mid, int[] piles, int h) {
-        int count = 0;
-        for (int pile: piles) {
-            if (count>h) return Integer.MAX_VALUE;
-            count+=Math.ceil((double)pile/mid);
+
+        while (left < right) {
+            // Get the middle index between left and right boundary indexes.
+            // hourSpent stands for the total hour Koko spends.
+            int middle = (left + right) / 2;
+            int hourSpent = 0;
+
+            // Iterate over the piles and calculate hourSpent.
+            // We increase the hourSpent by ceil(pile / middle)
+            for (int pile : piles) {
+                hourSpent += Math.ceil((double) pile / middle);
+            }
+
+            // Check if middle is a workable speed, and cut the search space by half.
+            if (hourSpent <= h) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
         }
-        return count;
+
+        // Once the left and right boundaries coincide, we find the target value,
+        // that is, the minimum workable eating speed.
+        return right;
     }
-    
 }
