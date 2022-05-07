5ed3972b6e1231bdc5961f5abc05cfab0ce7af1a
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-07 22:22:05 +0530

    Time: 1 ms (61.59%), Space: 42.5 MB (51.25%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,22 +1,21 @@
 class Solution {
-    public int search(int[] arr, int target) {
+    public int search(int[] nums, int target) {
         int start = 0;
-        int end = arr.length-1;
-        while (start<end) {
+        int end = nums.length-1;
+        while (start<=end) {
             int mid = start+(end-start)/2;
-            if (arr[mid] == target) return mid;
-            //Unsorted part (Right part is sorted)
-            if (arr[mid]<arr[start]) {
-                if (arr[mid]<target && arr[end]>=target) { //also make sure if it's inside that sorted part
-                    start = mid+1;
-                } else end = mid-1;
-            } 
-            //Sorted part (left part is sorted)
-            else {
-                if (arr[mid]>target && target>=arr[start]) end = mid-1;
+            //I'm in left sorted part
+            if (nums[mid] == target) return mid;
+            else if (nums[mid]>=nums[start]) {
+                if (target>=nums[start] && target<=nums[mid]) end = mid-1;
                 else start = mid+1;
-            }      
+            } 
+            //I'm in right sorted part
+            else {
+                if (target>=nums[mid] && nums[end]>=target) start = mid+1;
+                else end = mid-1;
+            }
         }
-        return arr[end]==target?end:-1;
+        return -1;
     }
 }
