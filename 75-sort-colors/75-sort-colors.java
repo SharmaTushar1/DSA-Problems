273587bc5320f821cab298215dd76162fea65a38
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-10 18:16:50 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (61.37%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,21 +1,21 @@
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num: nums) map.put(num, map.getOrDefault(num, 0)+1);
-        int i = 0;
-        if (map.containsKey(0)) {
-            for (int x = 0; x<map.get(0); x++) {
-                nums[i++] = 0;
-            }
-        }
-        if (map.containsKey(1)) {
-            for (int y = 0; y<map.get(1); y++) {
-                nums[i++] = 1;
-            }
-        }
-        if (map.containsKey(2)) {
-            for (int z = 0; z<map.get(2); z++) {
-                nums[i++] = 2;
+        int start = 0;
+        int end = nums.length-1;
+        int mid = 0;
+        while (mid<=end) {
+            if (nums[mid]==0) {
+                int temp = nums[mid];
+                nums[mid] = nums[start];
+                nums[start] = temp;
+                mid++;
+                start++;
+            } else if (nums[mid]==1) mid++;
+            else {
+                int temp = nums[mid];
+                nums[mid] = nums[end];
+                nums[end] = temp;
+                end--;
             }
         }
     }
