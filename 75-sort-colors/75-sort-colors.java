e5abe09f47b56ea91d01c6af56b0d4f0d8fcaa18
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-10 17:59:06 +0530

    Time: 1 ms (33.53%), Space: 40.7 MB (88.96%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,25 +1,27 @@
 class Solution {
-    public void sortColors(int[] arr) {
-        int lo = 0;
-        int hi = arr.length - 1;
-        int mid = 0;
-        while (mid<=hi) {
-            switch (arr[mid]) {
-            case 0: 
-                int temp = arr[lo];
-                arr[lo] = arr[mid];
-                arr[mid] = temp;
-                mid++; lo++;
-                break;
-            case 1: mid++;
-                break;
-            case 2:
-                int temp2 = arr[hi];
-                arr[hi] = arr[mid];
-                arr[mid] = temp2;
-                hi--;
-                break;
+    public void sortColors(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num: nums) map.put(num, map.getOrDefault(num, 0)+1);
+        int i = 0;
+        if (map.containsKey(0)) {
+            while (i<map.get(0)) {
+                nums[i] = 0;
+                i++;
+            }
         }
+        if (map.containsKey(1)) {
+            int j = 0;
+            while (j<map.get(1)) {
+                nums[i++] = 1;
+                j++;
+            }
+        }
+        if (map.containsKey(2)) {
+            int k = 0;
+            while (k<map.get(2)) {
+                nums[i++] = 2;
+                k++;
+            }
         }
     }
 }
