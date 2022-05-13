a35ff5a997edab006dcebe63d7876518fbffc47b
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-13 14:43:51 +0530

    Time: 1 ms (92.80%), Space: 47.3 MB (24.62%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -2,17 +2,24 @@ class Solution {
     public int trap(int[] height) {
         int area = 0;
         int trappedWater = 0;
+        
+        //create a leftPrefix array
+        int[] left = new int[height.length];
+        int l = 0;
+        for (int i =  0; i<height.length; i++) {
+            l = Math.max(l, height[i]);
+            left[i] = l;
+        }
+        //create a rightPrefix array
+        int[] right = new int[height.length];
+        int r = 0;
+        for (int i = height.length-1; i>=0; i--) {
+            r = Math.max(r, height[i]);
+            right[i] = r;
+        }
+        
         for (int i = 0; i<height.length; i++) {
-            int lHeight = 0;
-            int rHeight = 0;
-            //find lHeight
-            for (int j=i-1; j>=0; j--) 
-                lHeight = Math.max(lHeight, height[j]);
-            //find rHeight
-            for (int j = i+1; j<height.length; j++) 
-                rHeight = Math.max(rHeight, height[j]);
-            int abs = Math.min(lHeight, rHeight);
-            area = abs - height[i];
+            area = Math.min(left[i], right[i]) - height[i];
             trappedWater += Math.max(0, area);
         }
         return trappedWater;
