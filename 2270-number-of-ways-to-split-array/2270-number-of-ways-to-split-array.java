d17152fd47f5b44334c3b6c9a4beb53b80fb7df5
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-14 22:52:43 +0530

    Time: 6 ms (33.33%), Space: 82.4 MB (66.67%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,25 +1,14 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long[] left = new long[nums.length];
-        long lSum = 0;
-        for (int i = 0; i<nums.length; i++) {
-            lSum += nums[i];
-            left[i] = lSum;
-        }
-        System.out.println(Arrays.toString(left));
-        long[] right = new long[nums.length];
         long rSum = 0;
-        for (int j = nums.length-1; j>=0; j--) {
-            rSum += nums[j];
-            right[j] = rSum;
-        }
-        System.out.println(Arrays.toString(right));
-        int i = 0, j = 1;
+        for (int num: nums) rSum += num;
+        long lSum = 0;
         int ans = 0;
-        while (i<left.length && j<right.length) {
-            if (left[i]>=right[j]) ans++;
-            i++;
-            j++;
+        System.out.println(rSum);
+        for (int i = 0; i<nums.length-1; i++) {
+            lSum += nums[i];
+            rSum -= nums[i];
+            if (lSum>=rSum) ans++;
         }
         return ans;
     }
