fb3ee486bffe6566d56a524229faee0109fb7b8d
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-17 18:20:55 +0530

    Time: 9 ms (35.29%), Space: 43.8 MB (57.71%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> st = new LinkedList<>();
+        for (String s: path.split("/")) {
+            if (s.equals("..")) st.poll();
+            else if (!s.equals(".") && !s.equals("")) st.push(s);
+        }
+        if (st.size()==0) return "/";
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append("/").append(st.pollLast());
+        }
+        return sb.toString();
+    }
+}
