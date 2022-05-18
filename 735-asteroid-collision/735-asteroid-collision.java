9df3d70e8a05aa21ffed7308739a3222f5a9158e
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-18 22:37:55 +0530

    Time: 4 ms (92.48%), Space: 48.5 MB (30.82%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.java
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -2,18 +2,21 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Deque<Integer> st = new ArrayDeque<>();
         for (int asteroid: asteroids) {
-            if (asteroid>0) st.addLast(asteroid);
+            if (asteroid>0) 
+                st.addLast(asteroid);
             else {
-                while (!st.isEmpty() && st.peekLast()>0 && st.peekLast()<-asteroid) st.pollLast();
-                if (!st.isEmpty() && st.peekLast() == -asteroid) st.pollLast();
-                else if (st.isEmpty() || st.peekLast()<0) st.add(asteroid);
+                while (!st.isEmpty() && st.peekLast()>0 && st.peekLast()<-asteroid) 
+                    st.pollLast();
+                if (!st.isEmpty() && st.peekLast()==-asteroid) 
+                    st.pollLast();
+                else if (st.isEmpty() || st.peekLast()<0)
+                    st.addLast(asteroid);
             }
         }
         int[] ans = new int[st.size()];
         int i = 0;
-        while (!st.isEmpty()) {
+        while (!st.isEmpty()) 
             ans[i++] = st.pollFirst();
-        }
         return ans;
     }
 }
