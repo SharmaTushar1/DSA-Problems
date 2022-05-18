9afd15e7d12cd441d83f95ec9220c297017649e3
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-18 12:53:14 +0530

    Time: 10 ms (25.17%), Space: 41.4 MB (47.86%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -2,11 +2,10 @@ class Solution {
     public int arrangeCoins(int n) {
         int i = 1;
         while (n>0) {
-            n-=i;
             i++;
+            n-=i;
         }
-        System.out.println(i);
-        if (n<0) i--;
+        // if (n<0) i--;
         return i-1;
     }
 }
