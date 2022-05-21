6a54ba424f84520aedc393cd07b0af2690cb7ad0
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-05-21 18:14:35 +0530

    Time: 6 ms (16.69%), Space: 41.1 MB (71.50%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count = 0;
+        while (tickets[k]!=0) {
+            for (int i = 0; i<tickets.length; i++) {
+                if (tickets[k]==0) return count;
+                if (tickets[i]>0) {
+                    tickets[i]--;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
