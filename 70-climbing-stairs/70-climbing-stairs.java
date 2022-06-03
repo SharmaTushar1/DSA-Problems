727a995433a5c94184421def9cc113d468976391
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-03 08:27:40 +0530

    Time: 0 ms (100.00%), Space: 41 MB (39.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
         if (n<=2) return n;
-        int prev = 1;
-        int cur = 2;
+        int a = 1;
+        int b = 2;
         for (int i = 3; i<=n; i++) {
-            int later = prev+cur;
-            prev = cur;
-            cur = later;
+            int cur = a+b;
+            a = b;
+            b = cur;
         }
-        return cur;
+        return b;
     }
 }
