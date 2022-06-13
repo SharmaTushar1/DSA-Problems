432af24547bcdddc95f1256ea50347261ad66a3c
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-13 23:11:00 +0530

    Time: 2 ms, Memory: 55.5 MB - LeetHub

---
+++ reverse-string/reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        helper(0, s.length-1, s);
+    }
+    public void helper(int start, int end, char[] s) {
+        if (start>=end) 
+            return;
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        helper(start+1, end-1, s);
+    }
+}
