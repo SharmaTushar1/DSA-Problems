5415364cc4deb3ed9763f2f02f8ae4be12e257be
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-20 19:50:38 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (42.86%) - LeetHub

---
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if (n==1 || k==1) return 0;
+        //prevK is the max symbols possible in the prev row
+        int prevK = (int)Math.pow(2, n-2);//n-1 for current n-1-1 for prev
+        if (k<=prevK) 
+            return kthGrammar(n-1, k);
+        else {
+            int presK = k%prevK;
+            int val = kthGrammar(n-1, presK==0?k/2:k%prevK);
+            return val==1?0:1;
+        }
+    }
+}
