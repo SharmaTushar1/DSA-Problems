e99b7a0b7f2563da105dd86a6a4c24a4a046ffef
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-21 22:18:06 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (67.92%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,11 +1,17 @@
 class Solution {
     public int numTrees(int n) {
+        if (n==1 || n==2) return n;
+        int[] dp = new int[n+1];
+        helper(n, dp);
+        return dp[n];
+    }
+    public int helper(int n, int[] dp) {
         if (n==0) return 1;
         if (n==1 || n==2) return n;
-        int sum = 0;
+        if (dp[n]!=0) return dp[n];
         for (int i = 1; i<=n; i++) {
-            sum += numTrees(n-i)*numTrees(i-1);
+            dp[n] += helper(n-i, dp)*helper(i-1, dp);
         }
-        return sum;
+        return dp[n];
     }
 }
