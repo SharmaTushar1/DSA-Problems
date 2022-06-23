6fd13132986855dd5a497c2da2596f7f6596098f
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-23 11:01:21 +0530

    Time: 376 ms (14.38%), Space: 117.3 MB (11.38%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSquares(int n) {
+        Queue<Integer> q = new LinkedList<>();
+        //add visited array so we don't go to values which we have traversed already (similar to dp)
+        //we can avoid visited array but that'll improve the time complexity
+        HashSet<Integer> visited = new HashSet<>();
+        int ans = 0;
+        q.offer(n);
+        while (!q.isEmpty()) {
+            int size = q.size(); 
+            for (int i = 0; i<size; i++) {
+                int cur = q.poll();
+                if (cur == 0) return ans;
+                for (int j = 1; j<=cur/j; j++) {
+                    if (!visited.contains(cur-j*j)) {
+                        q.offer(cur-j*j);
+                        visited.add(cur-j*j);
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+}
