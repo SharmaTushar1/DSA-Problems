c8cc076079d8a2cf5300b5eb46dfdf3bcdae5ba8
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-23 16:04:20 +0530

    Time: 114 ms (13.63%), Space: 118.2 MB (31.96%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        HashMap<int[], Double> map = new HashMap<>();
-        PriorityQueue<Map.Entry<int[], Double>> pq = new PriorityQueue<>((a,b)->Double.compare(a.getValue(),b.getValue()));
+        HashMap<int[], Integer> map = new HashMap<>();
+        PriorityQueue<Map.Entry<int[], Integer>> pq = new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
         for (int i = 0; i<points.length; i++) {
-            map.put(points[i], Math.pow((Math.pow(points[i][0], 2)+Math.pow(points[i][1], 2)), 0.5));
+            map.put(points[i], (int)(Math.pow(points[i][0], 2)+Math.pow(points[i][1], 2)));
         }
         for (Map.Entry set: map.entrySet()) {
             pq.add(set);
