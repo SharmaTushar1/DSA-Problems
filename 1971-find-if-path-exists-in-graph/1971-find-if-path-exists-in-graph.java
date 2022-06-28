1b8e103c7333a7bc385c123b7c25dc046489a2fc
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-28 13:04:01 +0530

    Time: 393 ms (14.31%), Space: 198 MB (48.80%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -3,51 +3,31 @@ class Solution {
     
     boolean found = false;
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        
-        //construct graph
         HashMap<Integer, List<Integer>> graph = new HashMap<>();
-        
-        //structure the graph
-        
         for (int i = 0; i<n; i++) {
             graph.put(i, new ArrayList());
-        }
-        
-        //add values
-        
+        }        
         for (int[] edge: edges) {
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
-        
         HashSet<Integer> vis = new HashSet<>();
-        
-        // boolean found = false;
-        
-        dfs(graph, source, destination, vis);
-        
-        return found;
+        return dfs(graph, source, destination, vis, false);
         
     }
     
-    
-    
-    public void dfs(HashMap<Integer, List<Integer>> graph, int source, int destination, HashSet<Integer> vis) {
+    public boolean dfs(HashMap<Integer, List<Integer>> graph, int source, int destination, HashSet<Integer> vis, boolean ans) {
         if (source == destination) {
-            System.out.println("Dsf");
-            found = true;
-            return;
+            ans = true;
         }
-        vis.add(source);
-        for (int it: graph.get(source)) {
-            if (!vis.contains(it)) {
-                dfs(graph, it, destination, vis);
+        else {
+            vis.add(source);
+            for (int it: graph.get(source)) {
+                if (!vis.contains(it)) {
+                    ans = dfs(graph, it, destination, vis, ans);
+                }
             }
         }
-        // return false;
-    }
-    
-    
-    
-    
+        return ans;
+    } 
 }
