be1350cf8deefb2e3e0bac22969ee439d9ef1012
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-06-29 20:30:38 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (97.32%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int strStr(String s, String needle) {
+        if (needle.length()==0) return 0;
+        int m = s.length(), n = needle.length();
+        for (int i = 0; i<m; i++) {
+            if (s.charAt(i)==needle.charAt(0)) {
+                int temp = i;
+                int j = 0;
+                while (j<n && i<m && s.charAt(i)==needle.charAt(j)) {
+                    i++;
+                    j++;
+                }
+                if (j==n) return temp;
+                i = temp;
+            }
+        }
+        return -1;
+    }
+}
