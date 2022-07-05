a79cd1d1a7787b0e6066e45f3654179d8bdbe955
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-07-05 06:51:36 +0530

    Time: 3 ms (39.64%), Space: 45.7 MB (7.19%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -2,13 +2,12 @@ class Solution {
     public int maxProduct(int[] A) {
       int max = 1, min = 1, result = A[0];
         for (int i = 0; i < A.length; i++) {
-            //we need to keep track of the minimum value because multiplying it with the remaining value can give us the maximum value
+            //we need to keep track of the minimum value 
+            //because multiplying it with the remaining value can give us the maximum value
             int temp = max;
             max = Math.max(Math.max(max * A[i], min * A[i]), A[i]);
             min = Math.min(Math.min(temp * A[i], min * A[i]), A[i]);
-            if (max > result) {
-                result = max;
-            }
+            result = Math.max(result, max);
         }
         return result;
     }
