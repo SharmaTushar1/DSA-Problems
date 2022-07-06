3fde2b68a7faff29e8a0e7d1e962107675b6a6b3
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-07-06 09:50:40 +0530

    Time: 36 ms (77.77%), Space: 117.5 MB (50.45%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        HashMap<int[], Integer> map = new HashMap<>();
-        PriorityQueue<Map.Entry<int[], Integer>> pq = new PriorityQueue<>((a,b)->a.getValue()-b.getValue());
-        for (int i = 0; i<points.length; i++) {
-            map.put(points[i], (int)(Math.pow(points[i][0], 2)+Math.pow(points[i][1], 2)));
-        }
-        for (Map.Entry set: map.entrySet()) {
-            pq.add(set);
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->Integer.compare((a[0]*a[0]+a[1]*a[1]), (b[0]*b[0]+b[1]*b[1])));
+        for (int[] point: points) {
+            q.add(point);
         }
         int[][] ans = new int[k][2];
         for (int i = 0; i<k; i++) {
-            int[] temp = pq.poll().getKey();
-            ans[i][0] = temp[0];
-            ans[i][1] = temp[1];
+            int[] cur = q.poll();
+            ans[i][0] = cur[0];
+            ans[i][1] = cur[1];
         }
         return ans;
     }
