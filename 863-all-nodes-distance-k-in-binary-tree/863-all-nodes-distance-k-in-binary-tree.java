ec1d562a878856309ab004b4ff4eff1fd7e7c4b3
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-07-06 18:53:42 +0530

    Time: 16 ms (60.38%), Space: 42.9 MB (62.36%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -9,48 +9,37 @@
  */
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        HashMap<TreeNode, TreeNode> map = new HashMap<>();
-        //First bfs to make a mapping to the parent nodes 
-        Queue<TreeNode> q1 = new LinkedList<>();
-        q1.offer(root);
-        while (!q1.isEmpty()) {
-            int size = q1.size();
-            for (int i = 0;i<size;i++) {
-                TreeNode cur = q1.poll();
-                if(cur.left!=null) {
-                    q1.offer(cur.left);
-                    map.put(cur.left, cur);
-                }
-                if (cur.right!=null) {
-                    q1.offer(cur.right);
-                    map.put(cur.right, cur);
-                }
-            }
-        }
-        Queue<TreeNode> q2 = new LinkedList<>();
-        HashSet<TreeNode> vis = new HashSet<>();
         List<Integer> ans = new ArrayList<>();
-        int dis = 0;
-        q2.offer(target);
-        while (!q2.isEmpty()) {
-            int size = q2.size();
+        if (root == null) return ans;
+        HashMap<TreeNode, TreeNode> map = new HashMap<>();
+        graphMaker(root, null, map);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(target);
+        int distance = 0;
+        HashSet<TreeNode> vis = new HashSet<>();
+        while (!q.isEmpty()) {
+            int size = q.size();
             for (int i = 0; i<size; i++) {
-                TreeNode cur = q2.poll();
+                TreeNode cur = q.poll();
                 if (!vis.contains(cur)) {
-                    if (cur.left!=null) 
-                        q2.offer(cur.left);
-                    if (cur.right!=null)
-                        q2.offer(cur.right);
-                    if (map.get(cur)!=null) 
-                        q2.offer(map.get(cur));
-                    if (dis==k) {
+                    if (cur.left!=null) q.add(cur.left);
+                    if (cur.right!=null) q.add(cur.right);
+                    if (map.get(cur)!=null) q.add(map.get(cur));
+                    if (distance == k) 
                         ans.add(cur.val);
-                    }
                 }
                 vis.add(cur);
             }
-            dis++;
+            distance++;
         }
         return ans;
     }
+    
+    public void graphMaker(TreeNode cur, TreeNode parent, HashMap<TreeNode, TreeNode> map) {
+        if (cur==null) return;
+        map.put(cur, parent);
+        graphMaker(cur.left, cur, map);
+        graphMaker(cur.right, cur, map);
+    }
+    
 }
