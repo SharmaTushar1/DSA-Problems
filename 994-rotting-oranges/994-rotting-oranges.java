e11864243bdbd7104bb9514946e54c356a80a1e0
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-07-07 12:45:59 +0530

    Time: 3 ms (75.90%), Space: 42.5 MB (73.57%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,38 +1,35 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int rows = grid.length; 
-        int cols = grid[0].length;
+        int freshOranges = 0, minutes = 0;
         Queue<int[]> q = new LinkedList<>();
-        int freshOranges = 0;
-        //add all the positions which are rotten
-        for (int i = 0; i<rows; i++) {
-            for (int j = 0; j<cols; j++) {
-                if (grid[i][j]==2) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n; j++) {
+                if (grid[i][j] == 2) {
                     q.add(new int[]{i, j});
                 } else if (grid[i][j] == 1) {
                     freshOranges++;
                 }
             }
-        } 
+        }
         if (freshOranges == 0) return 0;
-        //apply bfs algorithm
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        int count = 0;
+        int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         while (!q.isEmpty()) {
-            ++count;
+            ++minutes;
             int size = q.size();
             for (int i = 0; i<size; i++) {
                 int[] cur = q.poll();
-                for (int j = 0; j<4; j++) {
+                for (int j =0; j<4; j++) {
                     int x = cur[0]+dirs[j][0];
                     int y = cur[1]+dirs[j][1];
-                    if (x<0 || y<0 || x>=rows || y>=cols || grid[x][y] == 0 || grid[x][y] == 2) continue;
+                    if (x<0 || y<0 || y>=n || x>=m || grid[x][y]==0 || grid[x][y] == 2) 
+                        continue;
                     grid[x][y] = 2;
                     q.add(new int[]{x, y});
                     freshOranges--;
                 }
             }
         }
-        return freshOranges==0?count-1:-1;
+        return freshOranges==0?minutes-1:-1;
     }
 }
