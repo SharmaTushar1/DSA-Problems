ffb89465aa8735958e699c29a9de16e294459e2f
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-07-16 14:46:37 +0530

    Time: 61 ms (53.98%), Space: 42.6 MB (87.22%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -5,13 +5,16 @@ class Solution {
         int maxLen = 0;
         String result = "";
         int n = s.length();
+        int fLeft = 0;
+        int fRight = 0;
         for (int i = 0; i<n; i++) {
             //odd cases
             left = i;
             right = i;
-            while (left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)) {
+            while (left>=0 && right<n && s.charAt(left) == s.charAt(right)) {
                 if (right-left+1 > maxLen) {
-                    result = s.substring(left, right+1);
+                    fLeft = left;
+                    fRight = right;
                     maxLen = right-left+1;
                 }
                 left--;
@@ -20,15 +23,17 @@ class Solution {
             //even cases
             left = i;
             right = i+1;
-            while (left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)) {
+            while (left>=0 && right<n && s.charAt(left) == s.charAt(right)) {
                 if (right-left+1 > maxLen) {
-                    result = s.substring(left, right+1);
+                    fLeft = left;
+                    fRight = right;
                     maxLen = right-left+1;
                 }
                 left--;
                 right++;
             }
         }
+        result = s.substring(fLeft, fRight+1);
         return result;
     }    
 }
