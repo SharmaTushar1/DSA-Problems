432634ccf83263f056f63d46cd948c33c19336a1
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-07-29 17:35:29 +0530

    Time: 7 ms (68.51%), Space: 48.9 MB (33.52%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,34 +1,37 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //make the adjacency list graph
-        HashMap<Integer, List<Integer>> adj = new HashMap<>();
         int[] inDegree = new int[numCourses];
-        for (int arr[]: prerequisites) {
-            int prev = arr[1];
-            int cur = arr[0];
-            var list = adj.getOrDefault(prev, new ArrayList<>());
-            list.add(cur);
-            inDegree[cur]++;
-            adj.put(prev, list);
+        HashMap<Integer, ArrayList<Integer>> adj = new HashMap<>();
+        for (int[] prerequisite: prerequisites) {
+            if (!adj.containsKey(prerequisite[1])) 
+                adj.put(prerequisite[1], new ArrayList<>());
+            adj.get(prerequisite[1]).add(prerequisite[0]);
+            inDegree[prerequisite[0]]++;
         }
         Queue<Integer> q = new LinkedList<>();
-        //Add the nodes with inDegree == 0
-        for (int i=0; i<inDegree.length; i++) 
-            if (inDegree[i] == 0) 
-                q.add(i);
-        //Kahn's algorithm
         int count = 0;
+        //add the numbers with indegree 0 in the queue
+        for (int i=0; i<inDegree.length; i++) {
+            if (inDegree[i]==0) {
+                q.add(i);
+                count++;
+            }
+        }
         while (!q.isEmpty()) {
-            int node = q.poll();
-            count++;
-            if (adj.containsKey(node)) {
-                for (int it: adj.get(node)) {
-                    inDegree[it]--;
-                    if (inDegree[it]==0) 
-                        q.add(it);
+            int level = q.size();
+            for (int i=0; i<level; i++) {
+                int cur = q.poll();
+                if (adj.containsKey(cur)) {
+                    for (int it: adj.get(cur)) {
+                        inDegree[it]--;
+                        if (inDegree[it]==0) {
+                            q.add(it);
+                            count++;
+                        }
+                    }
                 }
             }
         }
-        return numCourses == count;
+        return numCourses==count;
     }
 }
