3ad0445e96b1eb3aa1bdad4ac263a7934e4da609
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-08-01 17:17:09 +0530

    Time: 1 ms (55.39%), Space: 40.1 MB (84.51%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,18 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        
         int[][] dp = new int[m+1][n+1];
-        
-        return helper(m, n, dp);      
-        
-    }
-    
-    public int helper(int m, int n, int[][] dp) {
-        if (m==1 || n==1) return 1;
-        if (dp[m][n]!=0) return dp[m][n];
-        dp[m][n] = helper(m-1,n, dp)+helper(m,n-1, dp);
+        for (int[] d: dp) Arrays.fill(d, -1);
+        helper(m, n, dp);
         return dp[m][n];
     }
     
+    public int helper(int m, int n, int[][] dp) {
+        if (m==1 || n==1) return dp[m][n]=1;
+        if (dp[m][n]!=-1) return dp[m][n];
+        return dp[m][n] = helper(m-1, n, dp)+helper(m, n-1, dp);
+    }
     
 }
