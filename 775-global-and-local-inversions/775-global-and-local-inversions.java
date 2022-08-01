eab240bf414252023a1323a4fc949dd75e918028
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-08-01 12:25:48 +0530

    Time: 1 ms (100.00%), Space: 78.6 MB (49.67%) - LeetHub

---
+++ 775-global-and-local-inversions/775-global-and-local-inversions.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isIdealPermutation(int[] nums) {
+        int max = -1;
+        for (int i=0; i<nums.length-2; i++) {
+            max = Math.max(nums[i], max);
+            if (max>nums[i+2]) 
+                return false;
+        }
+        return true;
+    }
+}
