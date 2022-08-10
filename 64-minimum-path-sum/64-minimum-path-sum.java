f2caf2c980700349d575d9a8905417f8e4a97984
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-08-10 15:32:30 +0530

    Time: 3 ms (62.71%), Space: 46.4 MB (23.41%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,27 +1,24 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length-1;
-        int n = grid[0].length-1;
-        int[][] dp = new int[m+1][n+1];
-        for (int[] arr: dp) {
-            Arrays.fill(arr, -1);
+        int m=grid.length, n=grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (i==0 && j==0) {
+                    dp[0][0] = grid[0][0];
+                    continue;
+                }
+                if (i==0) {
+                    dp[i][j] = dp[i][j-1]+grid[i][j];
+                    continue;
+                } else if (j==0) {
+                    dp[i][j] = dp[i-1][j]+grid[i][j];
+                    continue;
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1])+grid[i][j];
+                }
+            }
         }
-        return helper(grid, m, n, dp);
+        return dp[m-1][n-1];
     }
-    
-    public int helper(int[][] grid,int m, int n, int[][] dp) {
-        if (m==0 && n==0) return grid[0][0];
-        if (m==0) {
-            dp[m][n] = grid[m][n] + helper(grid, m, n-1, dp);
-            return dp[m][n];
-        }
-        if (n==0) {
-            dp[m][n] = grid[m][n] + helper(grid, m-1, n, dp);
-            return dp[m][n];
-        }
-        if (dp[m][n]!=-1) return dp[m][n];
-        dp[m][n] = grid[m][n]+Math.min(helper(grid,m, n-1, dp), helper(grid, m-1, n, dp)); 
-        return dp[m][n];
-    }
-    
 }
