e735ae89980a298a52bae4f4b460bb2938ef4038
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2022-08-12 11:48:53 +0530

    Time: 2 ms (84.17%), Space: 41.9 MB (13.46%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,14 +1,12 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x<=1) return x;
-        int start = 0;
-        int end = x;
-        while (start<=end) {
-            int mid = start+(end-start)/2;
-            if (mid==x/mid) return mid;
-            else if (mid>x/mid) end = mid-1;
-            else start = mid+1;
+        int left = 1, right = x;
+        while (left<=right) {
+            int mid = left+(right-left)/2;
+            if (mid == x/mid) return mid;
+            else if (mid>x/mid) right = mid-1;
+            else left = mid+1;
         }
-        return end;
+        return right;
     }
 }
